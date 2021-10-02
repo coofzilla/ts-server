028d83536f7974cd317d5877c8bda9b10faead83
Jay Hernandez <jeric614@yahoo.com>
2021-10-02 10:13:52 +0900

    implemented interface f/dealing with poor type defs; commented out on loginRoutes keep only as ref

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -15,13 +15,53 @@ router.get('/login', (req: Request, res: Response) => {
     </div>
     <button>Submit</button>
   </form>
-  
   `);
 });
 
 router.post('/login', (req: Request, res: Response) => {
   const { email, password } = req.body;
-  res.send(email + password);
+
+  if (email) {
+    res.send(email.toUpperCase());
+  } else {
+    res.send('you must provide an email');
+  }
 });
 
 export { router };
+
+// import { Router, Request, Response } from 'express';
+
+// interface RequestWithBody extends Request {
+//   body: { [key: string]: string | undefined };
+// }
+
+// const router = Router();
+
+// router.get('/login', (req: Request, res: Response) => {
+//   res.send(`
+//   <form method="POST">
+//     <div>
+//       <label>Email</label>
+//       <input name="emailWrong"/>
+//     </div>
+//     <div>
+//       <label>Password</label>
+//       <input name="passwordWrong" type="password"
+//     </div>
+//     <button>Submit</button>
+//   </form>
+//   `);
+// });
+
+// router.post('/login', (req: RequestWithBody, res: Response) => {
+//   const { email, password } = req.body;
+
+//   if (email) {
+//     res.send(email.toUpperCase());
+//   } else {
+//     res.send('you must provide an email');
+//   }
+// });
+
+// export { router };
